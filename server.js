25287d7d10e9ca3efab1c2b7d7b0f11438f5660c
bobrikilya <ilya.bobrik.2020@gmail.com>
2023-10-14 16:43:26 +0300

    Connected CSS, SASS; Changed project's structure

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const path = require('path');
+// const morgan = require('morgan')
 
 const app = express();
+
 const PORT = 3000;
 
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+
+const createPath = (page) => path.resolve(__dirname, 'public', 'views', `${page}.html`);
   
 app.listen(PORT, (err) => {
   err ? console.log(err) : console.log(`Server listening port: ${PORT}`);
 });
 
-app.get('/', (req, res) =>{
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+// app.use(express.static('public'));
+app.use(express.static("public"));
+
+// app.use(express.static('styles'));
+
+app.get(['/', 'index.html'], (req, res) =>{
   res.sendFile(createPath('index'));
 });
