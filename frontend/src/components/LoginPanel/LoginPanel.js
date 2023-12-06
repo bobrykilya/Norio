b0f4fa3460b93814bdcc426f137a8d5cdf3e8660
bobrikilya <ilya.bobrik.2020@gmail.com>
2023-12-06 21:53:16 +0300

    Redesign, added new-user-but, titles

--- frontend/src/components/LoginPanel/LoginPanel.js
+++ frontend/src/components/LoginPanel/LoginPanel.js
@@ -19,13 +19,31 @@ function LoginPanel() {
     return (
         <div id='log-cont' className='cont'>
             <form id='sign-in-form' className='cont'>
-                <h1>Выполнить<br />вход</h1>
-                <div id='buts-cont'>
-                    <button id='info-but' type="button" className='cont'>
-                        <i className="fa-solid fa-info"></i>
+                <div id='enter-text-cont' className='cont'>
+                    <i class="fa-solid fa-door-open"></i>
+                    <h1>Выполнить<br />вход</h1>
+                </div>
+                <div id='buts-cont' className='cont'>
+                    <button
+                        id='new-user-but'
+                        title='Создать заявку на регистрацию нового пользователя'
+                        type="button"
+                        className='cont'>
+                            <i class="fa-regular fa-address-card"></i>
                     </button>
-                    <button id='pwa-but' type="button" className='cont'>
-                        <i className="fa-solid fa-desktop"></i>
+                    <button 
+                        id='pwa-but'
+                        title='Создать ярлык на рабочем столе'
+                        type="button"
+                        className='cont'>
+                            <i className="fa-solid fa-desktop"></i>
+                    </button>
+                    <button
+                        id='info-but'
+                        title='Описание приложения и инструкция'
+                        type="button"
+                        className='cont'>
+                        <i className="fa-solid fa-info"></i>
                     </button>
                 </div>
                 <div className='input-cont cont'>
